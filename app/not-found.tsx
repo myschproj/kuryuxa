92c4a71f853db72689e5e02fd8888aea0f1875e9
myschproj <150742169+myschproj@users.noreply.github.com>
2024-04-26 11:34:38 +0300

    Update not-found.tsx

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,7 +1,7 @@
 export default function NotFound() {
   return (
     <div className="flex items-center justify-center w-screen h-screen text-black">
-      потерялись?
+      потерялся в одностраничнике? ебать ты лох конечно
     </div>
   );
 }
